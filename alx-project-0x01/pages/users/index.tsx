597f63cdfa1f3ad40af3d94a08ba6d5aca81f52a
GH-Tyron <110431714+GH-Tyron@users.noreply.github.com>
2025-11-20 23:13:40 +0000

    Update index.tsx

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,1 +1,18 @@
+import UserCard from "@/components/common/UserCard";
+
+const UsersPage = ({ posts }: any) => {
+  return (
+    <div className="p-6">
+      <h1 className="text-3xl font-semibold mb-6">Users</h1>
+
+      <div className="grid md:grid-cols-3 gap-4">
+        {posts.map((user: any) => (
+          <UserCard key={user.id} user={user} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default UsersPage;
 
